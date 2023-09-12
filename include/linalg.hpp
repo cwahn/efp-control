fdeb837ce612df8c22e957ae0bd921d3405a438c
Chanwoo Ahn <cwahn@arsvivendi.io>
2023-09-12 18:09:13 +0900

    feat: Implement poly_coefficients with test for both real and complex

--- include/linalg.hpp
+++ include/linalg.hpp
@@ -1,12 +1,25 @@
 #ifndef LINALG_HPP_
 #define LINALG_HPP_
 
+#include "complex"
+
 #include "efp.hpp"
 #include "Eigen/Dense"
+#include "Eigen/Eigenvalues"
 
 namespace efp
 {
     using namespace Eigen;
+
+    template <typename A>
+    using Complex = typename std::complex<A>;
+
+    template <typename A, typename B>
+    bool floating_matrix_eq(const A &a, const B &b)
+    {
+        // todo float
+        return (a - b).norm() < std::sqrt(std::numeric_limits<double>::epsilon());
+    }
 
     template <typename A, typename B>
     B solve(const A &a, const B &b)
@@ -14,24 +27,115 @@ namespace efp
         return a.completeOrthogonalDecomposition().solve(b);
     }
 
-    template <typename Scalar, int degree>
-    Matrix<Scalar, degree + 1, 1> poly_coefficients(const Matrix<Scalar, degree, 1> &roots)
+    template <typename Scalar, int n>
+    auto eigenvalues(const Matrix<Scalar, n, n> &a)
+        -> Matrix<Complex<Scalar>, n, 1>
     {
-        Matrix<Scalar, degree + 1, 1> coeffs;
-        coeffs.setZero();
-
-        coeffs[0] = 1.0;
-
-        for (int i = 0; i < degree; ++i)
-        {
-            for (int j = degree; j >= 1; --j)
-            {
-                coeffs[j] = coeffs[j] - roots[i] * coeffs[j - 1];
-            }
-        }
-
-        return coeffs;
+        return EigenSolver<Matrix<Scalar, n, n>>{a}.eigenvalues();
     }
+
+    template <typename Scalar, int n>
+    Matrix<Scalar, n + 1, 1> poly_coefficients(const Matrix<Scalar, n, 1> &roots)
+    {
+        Matrix<Scalar, n + 1, 1> p;
+        p.setZero();
+        p[0] = (Scalar)1.;
+
+        const auto add_root = [&](int i, const Scalar &root)
+        {
+            const auto temp = from_function(i + 2,
+                                            [&](int i)
+                                            { return i == 0 ? 0. : root * p[i - 1]; });
+
+            for_each_with_index([&](int i, const Scalar &x)
+                                { p[i] -= x; },
+                                temp);
+        };
+
+        for_each_with_index(add_root, roots);
+
+        return p;
+    }
+
+    // template <typename A, typename B, typename C, typename D, int input = 0>
+    // auto ss_to_tf(const A &am, const B &bm, const C &cm, const D &dm)
+    // {
+    //     // A, B, C, D = abcd_normalize(A, B, C, D)
+
+    //     // nout, nin = D.shape
+    //     // if input >= nin:
+    //     //     raise ValueError("System does not have the input specified.")
+
+    //     // # make SIMO from possibly MIMO system.
+    //     // B = B[:, input:input + 1]
+    //     // D = D[:, input:input + 1]
+
+    //     // try:
+    //     //     den = poly(A)
+    //     // except ValueError:
+    //     //     den = 1
+
+    //     // if (prod(B.shape, axis=0) == 0) and (prod(C.shape, axis=0) == 0):
+    //     //     num = numpy.ravel(D)
+    //     //     if (prod(D.shape, axis=0) == 0) and (prod(A.shape, axis=0) == 0):
+    //     //         den = []
+    //     //     return num, den
+
+    //     // num_states = A.shape[0]
+    //     // type_test = A[:, 0] + B[:, 0] + C[0, :] + D + 0.0
+    //     // num = numpy.empty((nout, num_states + 1), type_test.dtype)
+    //     // for k in range(nout):
+    //     //     Ck = atleast_2d(C[k, :])
+    //     //     num[k] = poly(A - dot(B, Ck)) + (D[k] - 1) * den
+
+    //     // return num, den
+
+    //     // todo input dimension check
+
+    //     using NumType = typename D::Scalar;
+    //     using DenType = NumType;
+
+    //     constexpr int out_n = D::RowsAtCompileTime;
+    //     constexpr int in_n = D::ColsAtCompileTime;
+    //     static_assert(out_n < in_n, "System does not have the input specified.");
+
+    //     Matrix<typename B::Scalar, B::RowsAtCompileTime, 1> bv = bm.col(input);
+    //     Matrix<typename D::Scalar, D::RowsAtCompileTime, 1> dv = dm.col(input);
+
+    //     DenType den;
+    //     // try
+    //     // {
+    //     den = poly_coefficients(am);
+    //     // }
+    //     // catch (const std::exception &e)
+    //     // {
+    //     //     den = DenType(1);
+    //     // }
+
+    //     if (bv.prod() == 0 && cm.prod() == 0)
+    //     {
+    //         Matrix<NumType, nout, 1> num = dv.array();
+    //         if (dv.prod() == 0 && am.prod() == 0)
+    //         {
+    //             den = DenType(0);
+    //         }
+    //         return std::make_tuple(num, den);
+    //     }
+
+    //     constexpr int num_states = A::RowsAtCompileTime;
+    //     constexpr int num_outputs = nout;
+    //     Matrix<typename A::Scalar, num_states + 1, 1> num;
+
+    //     for (int k = 0; k < num_outputs; ++k)
+    //     {
+    //         Matrix<typename C::Scalar, 1, C::ColsAtCompileTime> Ck_row = cm.row(k);
+    //         Matrix<typename A::Scalar, num_states, 1> poly_arg = am - bm * Ck_row;
+    //         Matrix<NumType, num_states + 1, 1> num_k = poly_coefficients(poly_arg) + (dv(k) - NumType(1)) * den;
+    //         num.block(k * (num_states + 1), 0, num_states + 1, 1) = num_k;
+    //     }
+
+    //     return std::make_tuple(num, den);
+    // }
 }
 
 #endif
--- test/linalg_test.hpp
+++ test/linalg_test.hpp
@@ -17,15 +17,57 @@ TEST_CASE("solve")
     const Vector3d b{3., 3., 4.};
     const Vector3d x_ref{-2., 1., 1.};
 
-    CHECK((solve(a, b) - x_ref).norm() < double_tol);
+    // CHECK((solve(a, b) - x_ref).norm() < double_tol);
+    CHECK(floating_matrix_eq(solve(a, b), x_ref));
+}
+
+TEST_CASE("eigenvalues")
+{
+    const Matrix3d a{{2, 1, 0},
+                     {1, 3, 1},
+                     {0, 1, 2}};
+    const Vector3<std::complex<double>> ref{std::complex<double>{1., 0.},
+                                            std::complex<double>{2., 0.},
+                                            std::complex<double>{4., 0.}};
+
+    // CHECK((eigenvalues(a) - ref).norm() < double_tol);
+    CHECK(floating_matrix_eq(eigenvalues(a), ref));
 }
 
 TEST_CASE("poly_coefficients")
 {
-    const Vector3d a{-1. / 2, 0, 1. / 2};
-    const Vector4d ref{1., 0., -0.25, 0.};
+    SECTION("real")
+    {
+        const Vector3d roots{1., 1., 1.};
+        const Vector4d ref{1., -3., 3., -1};
 
-    CHECK((poly_coefficients(a) - ref).norm() < double_tol);
+        CHECK(floating_matrix_eq(poly_coefficients(roots), ref));
+        // CHECK(poly_coefficients(roots) == ref);
+    }
+
+    SECTION("complex")
+    {
+        const Vector2<Complex<double>> roots{
+            Complex<double>{1., 1.},
+            Complex<double>{1., -1.}};
+
+        const Vector3<Complex<double>> ref{
+            Complex<double>{1., 0.},
+            Complex<double>{-2., 0.},
+            Complex<double>{2., 0.},
+        };
+
+        CHECK(floating_matrix_eq(poly_coefficients(roots), ref));
+        // CHECK(poly_coefficients(roots) == ref);
+    }
 }
+
+// TEST_CASE("poly_coefficients")
+// {
+//     const Vector3d a{-1. / 2, 0, 1. / 2};
+//     const Vector4d ref{1., 0., -0.25, 0.};
+
+//     CHECK((poly_coefficients(a) - ref).norm() < double_tol);
+// }
 
 #endif
